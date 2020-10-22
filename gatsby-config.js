76a6ee40fcccf147c0c5d8d6c257137c67c3f267
Emerson Paiva <emersonaraujopaiva@gmail.com>
2020-10-22 09:46:59 -0300

    feat: add default packates to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,54 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: 'SITE_NAME',
+    description: 'SITE_DESCRIPTION',
+    siteUrl: 'http://localhost',
+    author: 'author',
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-root-import`,
+      options: {
+        src: `${__dirname}/src`,
+        pages: `${__dirname}/src/pages`,
+        components: `${__dirname}/src/components`,
+        styles: `${__dirname}/src/styles`,
+        assets: `${__dirname}/src/assets`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: `${__dirname}/src/assets/images/svgs`,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+        ignore: [`${__dirname}/src/assets/images/svgs`],
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-robots-txt`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `SITE_NAME`,
+        short_name: `SITE_NAME`,
+        start_url: `/`,
+        background_color: `#0e0634`,
+        theme_color: `#0e0634`,
+        display: `minimal-ui`,
+        icon: `src/assets/images/favicon.png`,
+      },
+    },
+  ],
 }
